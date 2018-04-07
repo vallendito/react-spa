7051457955e5374392ec29dcf3d6afaea1d86f77
vallendito <barlyvallendito@gmail.com>
2018-04-07 12:14:00 +0700

    add initial project

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return(
+            <div>
+                <h2>Hello</h2>
+                <p>Occaecat consectetur qui occaecat laboris quis 
+                consectetur qui elit nisi fugiat fugiat eiusmod. Fugiat 
+                reprehenderit eu pariatur consectetur non ea mollit id adipisicing 
+                velit culpa minim irure. Voluptate anim Lorem laborum reprehenderit 
+                ea voluptate sint ipsum sit excepteur. Magna adipisicing anim duis 
+                aliqua laborum officia nisi minim mollit magna cillum veniam 
+                consectetur. Esse ad laborum enim voluptate quis proident elit 
+                officia laborum ipsum sit minim. Excepteur irure duis elit velit 
+                esse officia commodo.</p>
+
+                <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+            </div>
+        );
+    }
+}
+
+export default home;
